b4f3f99c45dd20ffb1bf9d8a4405d262f24eea20
Nicklas <nnicklas92@hotmail.com>
2016-03-21 14:44:46 +0100

    Test funktion til BLL af Find hvilke forretninger der har en vare

--- Databaser/Pristjek220 med Interfaces/Pristjek220/Pristjek220.Unit.Test/ConsumerUnitTest.cs
+++ Databaser/Pristjek220 med Interfaces/Pristjek220/Pristjek220.Unit.Test/ConsumerUnitTest.cs
@@ -71,11 +71,19 @@ namespace Pristjek220.Unit.Test
         }
 
         [Test]
-        public void FindStoreAssortment_FindAldisAssortment_ReturnListWithAssortmentAndPrice()
+        public void FindStoreAssortment_FindAldisAssortment_FindProductsInStoreFunctionCalled()
         {
             _uut.FindStoresAssortment(_store.StoreName);
 
             _unitWork.Stores.Received(1).FindProductsInStore(_store.StoreName);
+        }
+
+        [Test]
+        public void FindStoreThatSells_FindWhichStoreSellsBanan_FunctionToGenerateListCalled()
+        {
+            _uut.FindStoresThatSellsProduct(_product.ProductName);
+
+            _unitWork.Products.Received(1).FindStoresThatSellsProduct(_product.ProductName);
         }
 
         [Test]
