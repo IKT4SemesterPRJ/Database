76adef82d3286c8a03d1c505ad8a0ee3f6acf1f9
Anders Meidahl Münsberg <201404246@post.au.dk>
2016-04-04 21:52:45 +0200

    Sorting efter forretning, vare på (Gereret indkøbsliste)

--- Databaser/Pristjek220 med Interfaces/Pristjek220/Consumer GUI/User Controls/GeneratedShoppingListModel.cs
+++ Databaser/Pristjek220 med Interfaces/Pristjek220/Consumer GUI/User Controls/GeneratedShoppingListModel.cs
@@ -1,26 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Consumer;
-using Prism.Events;
 using Pristjek220Data;
 
 namespace Consumer_GUI.User_Controls
 {
-    class GeneratedShoppingListModel : ObservableObject, IPageViewModel
+    internal class GeneratedShoppingListModel : ObservableObject, IPageViewModel
     {
         private UnitOfWork _unit = new UnitOfWork(new DataContext());
-        private IConsumer _user;
+        private readonly IConsumer _user;
 
         public GeneratedShoppingListModel(Consumer.Consumer user)
         {
             _user = user;
         }
 
-        public ObservableCollection<StoreProductAndPrice> GeneratedShoppingListData => _user.GeneratedShoppingListData;
+        public ObservableCollection<StoreProductAndPrice> GeneratedShoppingListData
+        {
+            get
+            {
+                return
+                    new ObservableCollection<StoreProductAndPrice>(
+                        _user.GeneratedShoppingListData.OrderBy(listData => listData.StoreName)
+                            .ThenBy(listData => listData.ProductName));
+            }
+        }
     }
 }
-
