607fea77da63c28fc0c2ee16bd856ff9478822c5
Mette Grønbech <201305561@post.au.dk>
2016-04-05 07:43:51 +0200

    Test af autocomplete lavet lidt mere simpel

--- Databaser/Pristjek220 med Interfaces/Pristjek220/Pristjek220.Unit.Test/AutoCompleteUnitTest.cs
+++ Databaser/Pristjek220 med Interfaces/Pristjek220/Pristjek220.Unit.Test/AutoCompleteUnitTest.cs
@@ -11,40 +11,15 @@ namespace Pristjek220.Unit.Test
     {
         private IUnitOfWork _unitWork;
         private Autocomplete.Autocomplete _uut;
-        private List<Store> _storesWithF;
-        private List<Store> _storesWithA; 
-        private List<Product> _productsWithB;
-        private List<Product> _productsWithK;
+        private List<Store> _stores;
+        private List<Product> _products;
 
         [SetUp]
         public void SetUp()
         {
             _unitWork = Substitute.For<IUnitOfWork>();
-            
-            _productsWithB = new List<Product>();
-            var bananProduct = new Product() {ProductName = "Banan"};
-            var baconProduct = new Product() {ProductName = "Bacon"};
-            var bollerProduct = new Product() {ProductName = "Boller"};
-            _productsWithB.Add(baconProduct);
-            _productsWithB.Add(bananProduct);
-            _productsWithB.Add(bollerProduct);
-
-            _productsWithK = new List<Product>();
-            var kakaoProduct = new Product() { ProductName = "Kakao" };
-            _productsWithK.Add(kakaoProduct);
-
-            _storesWithF = new List<Store>();
-            var faktaStore = new Store() {StoreName = "Fakta"};
-            var føtexStore = new Store() {StoreName = "Føtex"};
-            var fleggardStore = new Store() {StoreName = "Fleggard"};
-            _storesWithF.Add(faktaStore);
-            _storesWithF.Add(føtexStore);
-            _storesWithF.Add(fleggardStore);
-
-            _storesWithA = new List<Store>();
-            var aldiStore = new Store() { StoreName = "Aldi" };
-            _storesWithA.Add(aldiStore);
-
+            _products = new List<Product>();
+            _stores = new List<Store>();
             _uut = new Autocomplete.Autocomplete(_unitWork);
         }
 
@@ -65,14 +40,26 @@ namespace Pristjek220.Unit.Test
         [Test]
         public void AutoCompleteProduct_LookUpWordStartsWithB_ReturnListOfSize3()
         {
-            _unitWork.Products.FindProductStartingWith("B").Returns(_productsWithB);
+            var bananProduct = new Product() { ProductName = "Banan" };
+            var baconProduct = new Product() { ProductName = "Bacon" };
+            var bollerProduct = new Product() { ProductName = "Boller" };
+            var brombaerProduct = new Product() { ProductName = "Brombaer" };
+            _products.Add(baconProduct);
+            _products.Add(bananProduct);
+            _products.Add(bollerProduct);
+            _products.Add(brombaerProduct);
+
+            _unitWork.Products.FindProductStartingWith("B").Returns(_products);
             Assert.That(_uut.AutoCompleteProduct("B").Count, Is.EqualTo(3));
         }
 
         [Test]
         public void AutoCompleteProduct_LookUpWordStartsWithK_ReturnListOfSize1()
         {
-            _unitWork.Products.FindProductStartingWith("K").Returns(_productsWithK);
+            var kakaoProduct = new Product() { ProductName = "Kakao" };
+            _products.Add(kakaoProduct);
+
+            _unitWork.Products.FindProductStartingWith("K").Returns(_products);
             Assert.That(_uut.AutoCompleteProduct("K").Count, Is.EqualTo(1));
         }
 
@@ -93,14 +80,26 @@ namespace Pristjek220.Unit.Test
         [Test]
         public void AutoCompleteStore_LookUpWordStartsWithF_ReturnListOfSize3()
         {
-            _unitWork.Stores.FindStoreStartingWith("F").Returns(_storesWithF);
+            var faktaStore = new Store() { StoreName = "Fakta" };
+            var føtexStore = new Store() { StoreName = "Føtex" };
+            var fleggardStore = new Store() { StoreName = "Fleggard" };
+            var fantasiStore = new Store() { StoreName = "Fantasi" };
+            _stores.Add(faktaStore);
+            _stores.Add(føtexStore);
+            _stores.Add(fleggardStore);
+            _stores.Add(fantasiStore);
+
+            _unitWork.Stores.FindStoreStartingWith("F").Returns(_stores);
             Assert.That(_uut.AutoCompleteStore("F").Count, Is.EqualTo(3));
         }
 
         [Test]
         public void AutoCompleteStore_LookUpWordStartsWithA_ReturnListOfSize1()
         {
-            _unitWork.Stores.FindStoreStartingWith("A").Returns(_storesWithA);
+            var aldiStore = new Store() { StoreName = "Aldi" };
+            _stores.Add(aldiStore);
+
+            _unitWork.Stores.FindStoreStartingWith("A").Returns(_stores);
             Assert.That(_uut.AutoCompleteStore("A").Count, Is.EqualTo(1));
         }
     }
