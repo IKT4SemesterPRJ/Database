3c0164c2263e60f4aea9111f721fc1c17263dc16
Nicklas <nnicklas92@hotmail.com>
2016-04-15 14:37:38 +0200

    Lavet funktioner i Admin til at tilføje en forretning til databasen og et login

--- Pristjek220/Administration/Admin.cs
+++ Pristjek220/Administration/Admin.cs
@@ -1,7 +1,37 @@
-﻿namespace Administration
+﻿using System.Security;
+using Pristjek220Data;
+
+namespace Administration
 {
     public class Admin : IAdmin
     {
-        
+        private readonly IUnitOfWork _unitOfWork;
+
+        public Admin(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void CreateLogin(string userName, string password, string storeName)
+        {
+            var store = _unitOfWork.Stores.FindStore(storeName);
+            var securedpassword = new SecureString();
+
+            foreach (var character in password)
+            {
+                securedpassword.AppendChar(character);
+            }
+
+            var login = new Pristjek220Data.Login() {Username = userName, Store = store};
+            _unitOfWork.Logins.Add(login);
+            _unitOfWork.Complete();
+        }
+
+        public void AddStore(Store store)
+        {
+            store.StoreName = char.ToUpper(store.StoreName[0]) + store.StoreName.Substring(1).ToLower();
+            _unitOfWork.Stores.Add(store);
+            _unitOfWork.Complete();
+        }
     }
 }
--- Pristjek220/Administration/IAdmin.cs
+++ Pristjek220/Administration/IAdmin.cs
@@ -1,7 +1,10 @@
-﻿namespace Administration
+﻿using Pristjek220Data;
+
+namespace Administration
 {
     public interface IAdmin
     {
-
+        void CreateLogin(string userName, string password, string storeName);
+        void AddStore(Store store);
     }
 }
