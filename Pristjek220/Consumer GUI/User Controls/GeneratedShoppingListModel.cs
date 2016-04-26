49ca4d9f0f19d04df0dcd051af994c629559b5a9
Anders Meidahl Münsberg <201404246@post.au.dk>
2016-04-26 14:37:06 +0200

    Opdatering af Mail, med knap og ændret på opsætning af hvordan mailen ser ud visuelt

--- Pristjek220/Consumer GUI/User Controls/GeneratedShoppingListModel.cs
+++ Pristjek220/Consumer GUI/User Controls/GeneratedShoppingListModel.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Drawing.Text;
 using System.Linq;
+using System.Net.Mail;
+using System.Windows.Documents;
+using System.Windows.Input;
 using Consumer;
 using Pristjek220Data;
 
@@ -11,10 +16,13 @@ namespace Consumer_GUI.User_Controls
         private UnitOfWork _unit = new UnitOfWork(new DataContext());
         private readonly IConsumer _user;
         public string TotalSum => _user.TotalSum;
+        private readonly Mail _mail;
+        private ICommand _sendMailCommand;
 
         public GeneratedShoppingListModel(Consumer.Consumer user)
         {
             _user = user;
+            _mail = new Mail();
         }
 
         public ObservableCollection<StoreProductAndPrice> GeneratedShoppingListData
@@ -36,6 +44,29 @@ namespace Consumer_GUI.User_Controls
                     new ObservableCollection<ProductInfo>(
                         _user.NotInAStore.OrderBy(listData => listData.Name));
             }
+        }
+
+        public ICommand SendMailCommand
+        {
+            get
+            {
+                return _sendMailCommand ?? (_sendMailCommand = new RelayCommand(SendMail));
+            }
+        }
+
+        private void SendMail()
+        {
+            List<StoreProductAndPrice> test = new List<StoreProductAndPrice>();
+            StoreProductAndPrice test2 = new StoreProductAndPrice();
+            test2.StoreName = "asd";
+            test2.Price = 12;
+            test2.ProductName = "Tis";
+            test2.Quantity = "3";
+            test2.Sum = 23;
+            test.Add(test2);
+            test.Add(test2);
+
+            _mail.SendMail("AndersMeidahl@gmail.com", test, 252);
         }
     }
 }
