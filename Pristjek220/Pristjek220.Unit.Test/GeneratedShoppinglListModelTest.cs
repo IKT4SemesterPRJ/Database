4a7f524dd75e40213b41a213ef5780810e586c50
Anders Meidahl Münsberg <201404246@post.au.dk>
2016-05-10 21:18:30 +0200

    TEST af Usercontrolls, GeneratedShoppinglListModel (Det er noget lort at teste)

--- Pristjek220/Pristjek220.Unit.Test/GeneratedShoppinglListModelTest.cs
+++ Pristjek220/Pristjek220.Unit.Test/GeneratedShoppinglListModelTest.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using Consumer;
 using Consumer_GUI.User_Controls;
 using NSubstitute;
@@ -56,6 +58,56 @@ namespace Pristjek220.Unit.Test
             _uut.SendMailCommand.Execute(this);
             Thread.Sleep(500); //Den kalder en baggrundstråd, så den skal lige være kaldt.
             _mail.Received(1).SendMail("test@sesese.dk", Arg.Any<ObservableCollection<StoreProductAndPrice>>(), Arg.Any<ObservableCollection<ProductInfo>>(), "22");
+        }
+
+        [Test]
+        public void StoreChanged_ChangeStoreToStore0FromStore1_ErrorStoreIsEmptyString()
+        {
+            StoreProductAndPrice storeProductAndPrice = new StoreProductAndPrice()
+            {
+                ProductName = "Test",
+                Price = 2,
+                Quantity = "2",
+                StoreName = "Store1"
+            };
+
+            _uut.ErrorStore = "Test";
+            _uut.SelectedIndexGeneratedShoppingList = 0;
+            _uut.SelectedStoreIndex = 0;
+            _user.StoreNames = new List<string>();
+            _user.GeneratedShoppingListData = new ObservableCollection<StoreProductAndPrice>() { storeProductAndPrice };
+            _uut.StoreNames.Add("Store0");
+            _uut.StoreNames.Add("Store1");
+            _uut.GeneratedShoppingListData.Add(storeProductAndPrice);
+            _user.ChangeItemToAnotherStore("Store0", storeProductAndPrice).Returns(1);
+
+            _uut.StoreChangedCommand.Execute(this);
+            Assert.That(_uut.ErrorStore, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void StoreChanged_ChangeStoreToStore0FromStore1WhereStore0DontHaveProduct_ErrorStoreIsStringWithMessage()
+        {
+            StoreProductAndPrice storeProductAndPrice = new StoreProductAndPrice()
+            {
+                ProductName = "Test",
+                Price = 2,
+                Quantity = "2",
+                StoreName = "Store1"
+            };
+
+            _uut.ErrorStore = "Test";
+            _uut.SelectedIndexGeneratedShoppingList = 0;
+            _uut.SelectedStoreIndex = 0;
+            _user.StoreNames = new List<string>();
+            _user.GeneratedShoppingListData = new ObservableCollection<StoreProductAndPrice>() {storeProductAndPrice};
+            _uut.StoreNames.Add("Store0");
+            _uut.StoreNames.Add("Store1");
+            _uut.GeneratedShoppingListData.Add(storeProductAndPrice);
+            _user.ChangeItemToAnotherStore("Store0", storeProductAndPrice).Returns(0);
+
+            _uut.StoreChangedCommand.Execute(this);
+            Assert.That(_uut.ErrorStore, Is.EqualTo("Store0 har ikke Test i deres sortiment"));
         }
     }
 }
