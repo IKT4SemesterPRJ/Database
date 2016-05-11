332b3cf25961f3f9befd090ca189d77c80cc9620
Anders Meidahl Münsberg <201404246@post.au.dk>
2016-05-11 20:36:21 +0200

    Timers smidt på labels i hele Consumer GUI, samt test af tilhørende funktioner

--- Pristjek220/Pristjek220.Unit.Test/GeneratedShoppinglListModelTest.cs
+++ Pristjek220/Pristjek220.Unit.Test/GeneratedShoppinglListModelTest.cs
@@ -138,5 +138,57 @@ namespace Pristjek220.Unit.Test
             _user.MoneySaved = "Test";
             Assert.That(_uut.MoneySaved, Is.EqualTo("Test"));
         }
+
+        [Test]
+        public void NotInAStore_SetToBanan_IsBanan()
+        {
+            _user.NotInAStore = new ObservableCollection<ProductInfo>() {new ProductInfo("Banan")};
+            Assert.That(_uut.NotInAStore[0].Name, Is.EqualTo("Banan"));
+        }
+        
+
+        [Test]
+        public void StoreChanged_AssertThatWhenSelectedIndexGenShoppingListIsMinus1NoChangeHappenToErrorStore_ErrorStoreEqualNoChange()
+        {
+            _uut.ErrorStore = "No Change";
+            _uut.SelectedIndexGeneratedShoppingList = -1;
+            _uut.StoreChangedCommand.Execute(this);
+            Assert.That(_uut.ErrorStore, Is.EqualTo("No Change"));
+        }
+
+        [Test]
+        public void ErrorText_WithEmptyEmailwait5Sek_ErrorTextIsEmpty()
+        {
+            _uut.EmailAddress = "";
+
+            _uut.SendMailCommand.Execute(this);
+            Thread.Sleep(5000);
+            Assert.That(_uut.ErrorText, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void ErrorStore_ChangeStoreToStore0FromStore1WhereStore0DontHaveProductWait5Sek_ErrorStoreIsEmpty()
+        {
+            StoreProductAndPrice storeProductAndPrice = new StoreProductAndPrice()
+            {
+                ProductName = "Test",
+                Price = 2,
+                Quantity = "2",
+                StoreName = "Store1"
+            };
+
+            _uut.ErrorStore = "Test";
+            _uut.SelectedIndexGeneratedShoppingList = 0;
+            _uut.SelectedStoreIndex = 0;
+            _user.StoreNames = new List<string>();
+            _user.GeneratedShoppingListData = new ObservableCollection<StoreProductAndPrice>() { storeProductAndPrice };
+            _uut.StoreNames.Add("Store0");
+            _uut.StoreNames.Add("Store1");
+            _uut.GeneratedShoppingListData.Add(storeProductAndPrice);
+            _user.ChangeItemToAnotherStore("Store0", storeProductAndPrice).Returns(0);
+
+            _uut.StoreChangedCommand.Execute(this);
+            Assert.That(_uut.ErrorStore, Is.EqualTo("Store0 har ikke produktet \"Test\" i deres sortiment."));
+        }
     }
 }
